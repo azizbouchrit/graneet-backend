7684ceb14c3fb7867acd88505d5249b34847d2ca
azizbouchrit <medazizbouchrit@gmail.com>
2021-10-14 13:59:36 +0200

    connected to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { CityModule } from './city/city.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/graneet'), CityModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://root:root@cluster0.vl6v6.mongodb.net/graneet?retryWrites=true&w=majority'), CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
